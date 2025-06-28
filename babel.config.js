dbb5a6f4bbdfdd24dc0e2043d6b50b957f89a7df
sbhjt-gr <sg_outlp@outlook.com>
2025-06-28 14:22:05 +0530

    build: auto-remove console logs in production builds

--- babel.config.js
+++ babel.config.js
@@ -14,7 +14,10 @@ module.exports = function(api) {
     ],
     env: {
       production: {
-        plugins: ['react-native-paper/babel'],
+        plugins: [
+          'react-native-paper/babel',
+          'transform-remove-console'
+        ],
       },
     },
   };
