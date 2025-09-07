0eff6c978a4f6f9038f037d4b1c855006e7ac48b
sbhjt-gr <sg_outlp@outlook.com>
2025-09-07 21:57:43 +0530

    fix(icon): add adaptive icon

--- app.config.js
+++ app.config.js
@@ -51,7 +51,7 @@ export default {
     android: {
       versionCode: 260,
       adaptiveIcon: {
-        foregroundImage: "./assets/icon.png",
+        foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#660880"
       },
       package: "com.gorai.ragionare",
