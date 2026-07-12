338af8146e1f516c088f9daaacbdd97c0c296595
Subhajit Gorai <47049949+cacheoverlord@users.noreply.github.com>
2026-07-12 18:02:54 +0530

    fix(app-functions): add module-local ksp classpath

--- modules/app-functions/plugin/index.js
+++ modules/app-functions/plugin/index.js
@@ -1,29 +1,7 @@
-const {
-  withAppBuildGradle,
-  withProjectBuildGradle,
-  createRunOncePlugin,
-} = require('@expo/config-plugins');
-
-function addKspClasspath(contents) {
-  if (contents.includes('com.google.devtools.ksp')) {
-    return contents;
-  }
-  if (contents.includes('dependencies {')) {
-    return contents.replace(
-      /dependencies\s*\{/,
-      `dependencies {\n        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.0.21-1.0.28")`,
-    );
-  }
-  return `${contents}\nbuildscript {\n  dependencies {\n    classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.0.21-1.0.28")\n  }\n}\n`;
-}
+const { withAppBuildGradle, createRunOncePlugin } = require('@expo/config-plugins');
 
 function withAppFunctions(config) {
-  config = withProjectBuildGradle(config, mod => {
-    mod.modResults.contents = addKspClasspath(mod.modResults.contents);
-    return mod;
-  });
-
-  config = withAppBuildGradle(config, mod => {
+  return withAppBuildGradle(config, mod => {
     if (!mod.modResults.contents.includes('compileSdkVersion')) {
       mod.modResults.contents = mod.modResults.contents.replace(
         /targetSdkVersion.*/,
@@ -32,8 +10,6 @@ function withAppFunctions(config) {
     }
     return mod;
   });
-
-  return config;
 }
 
 module.exports = createRunOncePlugin(withAppFunctions, 'app-functions', '1.0.0');
